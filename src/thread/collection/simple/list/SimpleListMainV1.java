f6f11bf367d6365df0d75f3f7f27ca9eea19edcd
cyh7834 <cyh7834@gmail.com>
2025-02-18 21:30:12 +0900

    동시성 컬렉션이 필요한 이유3 - 동기화

---
+++ src/thread/collection/simple/list/SimpleListMainV1.java
@@ -0,0 +1,14 @@
+package thread.collection.simple.list;
+
+import java.util.ArrayList;
+
+public class SimpleListMainV1 {
+    public static void main(String[] args) {
+        BasicList basicList = new BasicList();
+
+        basicList.add("A");
+        basicList.add("B");
+
+        System.out.println(basicList);
+    }
+}
